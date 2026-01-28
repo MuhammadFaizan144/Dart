96dc1076b839412b903490866f3b1d8fd56f6b6a
MuhammadFaizan144 <fg7829098@gmail.com>
2026-01-28 08:03:51 +0500

    initialize oops

---
+++ oops_dart/bin/oops_dart.dart
@@ -0,0 +1,5 @@
+import 'package:oops_dart/oops_dart.dart' as oops_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${oops_dart.calculate()}!');
+}
---
+++ oops_dart/lib/class&object.dart
@@ -0,0 +1,12 @@
+class addition {
+  add() {
+    int a = 28, b = 50, c;
+    c = a + b;
+    print(c);
+  }
+}
+
+void main() {
+  addition a = addition();
+  a.add();
+}
