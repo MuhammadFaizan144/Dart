14cde4a5de524dbce4d897bd2a7a5879ac7591d3
MuhammadFaizan144 <fg7829098@gmail.com>
2026-01-28 07:17:44 +0500

    learn loop

---
+++ dart_application_2/lib/loop.dart
@@ -0,0 +1,17 @@
+void main() {
+  // for (int i = 1; i <= 10; i++) {
+  //   print(i);
+  // }
+
+  // int i = 1;
+  // while (i <= 10) {
+  //   print(i);
+  //   i++;
+  // }
+
+  int i = 1;
+  do {
+    print(i);
+    i++;
+  } while (i <= 10);
+}
